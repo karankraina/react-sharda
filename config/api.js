333631396478a746bd33b0c27257a7b5e1e41669
Karan Raina <karanraina1996@gmail.com>
2021-05-27 14:43:53 +0530

    FIXED API ENDPOINT

--- config/api.js
+++ config/api.js
@@ -1,9 +1,9 @@
 // Dev endpoint for testing
-export const API_ENDPOINT = 'http://192.168.1.6:3000/api/mobile/'
-export const APP_ENDPOINT = 'http://192.168.1.6:3000/'
-export const API_HOST = 'http://192.168.1.14:3000/api/mobile/'
+export const API_ENDPOINT11 = 'http://192.168.1.6:3000/api/mobile/'
+export const APP_ENDPOINT11 = 'http://192.168.1.6:3000/'
+export const API_HOST11 = 'http://192.168.1.14:3000/api/mobile/'
 
 // Production Endpoint
-export const API_ENDPOINT1 = 'https://sharda.vercel.app/api/mobile/'
-export const API_HOST1 = 'https://sharda.vercel.app/api/mobile/'
-export const APP_ENDPOINT1 = 'https://sharda.vercel.app/'
+export const API_ENDPOINT = 'https://sharda.vercel.app/api/mobile/'
+export const API_HOST = 'https://sharda.vercel.app/api/mobile/'
+export const APP_ENDPOINT = 'https://sharda.vercel.app/'
